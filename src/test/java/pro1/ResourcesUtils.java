384baeca550d83a0f75fbd12e95a4693678e28e8
Tomáš Šafařík <safarto1@uhk.cz>
2026-05-04 08:37:23 +0200

    Support external test resource layout

--- src/test/java/pro1/ResourcesUtils.java
+++ src/test/java/pro1/ResourcesUtils.java
@@ -2,16 +2,29 @@ package pro1;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 public class ResourcesUtils {
     public static String readResourceFile(String path){
         try {
-            return Files.readString(Paths.get("src","test", "resources", path));
+            return readFirstExisting(
+                    Paths.get("src", "test", "resources", path),
+                    Paths.get("test", "resources", path)
+            );
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String readFirstExisting(Path... paths) throws IOException {
+        for (var candidate : paths) {
+            if (Files.exists(candidate)) {
+                return Files.readString(candidate);
+            }
+        }
+        throw new IOException("Resource file not found in supported locations");
     }
 }
 
